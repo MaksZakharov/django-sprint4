2ced09764dfe6f8ae9351c997d6d3ba97e0d272e
Maks Zakharov <zaharovm1982@mail.ru>
2025-05-14 10:37:31 +0500

    Изменение пагинации

--- blogicum/users/urls.py
+++ blogicum/users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'users'
+
 urlpatterns = [
     path('edit_profile/', views.edit_profile, name='edit_profile'),
     path('<str:username>/', views.profile, name='profile'),
