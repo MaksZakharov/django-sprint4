e3db3ea012eadd0a2e300917380969c91b0b5595
Maks Zakharov <zaharovm1982@mail.ru>
2025-05-14 09:41:21 +0500

    Перенес шаблоны users и profile в папку users

--- blogicum/users/views.py
+++ blogicum/users/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth import get_user_model
+from blog.models import Post  # если модель постов находится в blog
 
-# Create your views here.
+User = get_user_model()
+
+def profile(request, username):
+    user = get_object_or_404(User, username=username)
+    posts = Post.objects.filter(author=user).order_by('-pub_date')
+    return render(request, 'users/profile.html', {
+        'profile_user': user,
+        'posts': posts,
+    })
