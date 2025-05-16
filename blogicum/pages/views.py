a882963b28f2a8079aa677d1d0f9410c5b00d56c
Maks Zakharov <zaharovm1982@mail.ru>
2025-05-16 12:37:24 +0500

    Исправления

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -1,8 +1,9 @@
-# pages/views.py
 from django.views.generic import TemplateView
+
 
 class AboutView(TemplateView):
     template_name = 'pages/about.html'
+
 
 class RulesView(TemplateView):
     template_name = 'pages/rules.html'
