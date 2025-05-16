de2e5b3889a84ee22e0468522b3b7c34ca86b84d
Maks Zakharov <zaharovm1982@mail.ru>
2025-05-16 12:36:32 +0500

    Исправления

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path
-from django.conf.urls import handler403, handler404, handler500
+from django.conf.urls import handler404, handler500
 from blog import views
 from django.conf import settings
 from django.conf.urls.static import static
 
-handler404 = 'blog.views.page_not_found'  # Для кастомной 404
-handler500 = 'blog.views.server_error'  # Для кастомной 500
+handler404 = 'blog.views.page_not_found'
+handler500 = 'blog.views.server_error'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -18,4 +18,7 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
